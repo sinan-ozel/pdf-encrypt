bc43d7fd9abebd5b0f7b3f2c1874f81d3397486f
sinan-ozel <4183117+sinan-ozel@users.noreply.github.com>
2022-01-22 17:50:04 +0000

    Add URL to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='pdf-encrypt',
     description='A cross-platform tool to encrypt PDF files.',
     version='0.2.0',
+    url='https://github.com/sinan-ozel/pdf-encrypt',
     author='Sinan Ozel',
     license='MIT',
     packages=['PyPDF2'],
